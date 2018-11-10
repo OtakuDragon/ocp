6f77cc26bb1f93dce9f34b88d58663a1924d7157
OtakuDragon <programmer.gilson@gmail.com>
2018-11-10 18:12:27 -0200

    estudo ocp

--- src/main/java/OCP.java
+++ src/main/java/OCP.java
@@ -1,5 +1,10 @@
-import java.io.*;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class OCP {
 
@@ -62,13 +67,156 @@ public class OCP {
         catch block calling e.getSupressed(), if the only exception came from a close() method it goes straight to
         the catch block.
 
-        */
+        ----------------------------------------------------------------------------------------------------------
+        Dates, Numbers, Currencies e Locales
 
-        try(B b = new B();A a = new A()){
-            throw new IOException("1");
-        } catch (IOException  e) {
-            System.out.println(e.getSuppressed());
+        Date date = new Date();//Agora
+        Date date = new Date(0l);//01/01/1970
+
+        long millis = date.getTime();//Millisegundos desde 01/01/1970
+
+        Locale locale = new Locale("linguagem", "pais");
+        Locale locale = new Locale("pt", "BR");//Português Brasil
+        Locale locale = new Locale("pt");//Português
+        Locale locale = Locale.getDefault();//Locale padrão da JVM
+        Locale.setDefault(new Locale("en", "US"));//Muda o Locale padrão da JVM
+        locale.getDisplayCountry();
+        locale.getDisplayLanguage();
+
+        Calendar calendar = Calendar.getInstance();//No Locale padrão
+        Calendar calendar = Calendar.getInstance(locale);//No Locale passado
+
+        Date date = calendar.getTime();
+        calendar.setTime(date.getTime());
+        calendar.add(Calendar.DAY_OF_MONTH, 1);// +1 Dia mudando os outros campos
+        calendar.add(Calendar.DAY_OF_MONTH, -1);// -1 Dia mudando os outros campos
+        calendar.roll(Calendar.DAY_OF_MONTH, 1);// +1 Dia sem mudar os outros campos
+        calendar.roll(Calendar.DAY_OF_MONTH, -1);// -1 Dia sem mudar os outros campos
+
+        DateFormat dateFormat = DateFormat.getInstance();
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+
+        Date date = new Date();
+        String dateString = dateFormat.format(date);
+        try {
+            Date parsedDate = dateFormat.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
+
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        NumberFormat numberFormat = NumberFormat.getNumberInstance();
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
+
+        numberFormat.getMaximumFractionDigits();
+        numberFormat.setParseIntegerOnly(boolean);
+
+        double number = 1000;
+        String formattedNumber = numberFormat.format(number);
+        try {
+            Number parsedNumber = numberFormat.parse(formattedNumber);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        ----------------------------------------------------------------------------------------------------------
+        Parsing, Tokenizing and Formatting
+
+        Regex Metacharacters
+
+        \d (digit, 0-9)
+        \D (not digit, 0-9)
+        \s (whitespace, \t, \n, \f, \r)
+        \S (not whitespace, \t, \n, \f, \r)
+        \w (word character, a-z, A-Z, _)
+        \W (not word character, a-z, A-Z, _)
+        \b (word boundary, antes ou depois de um word character, mesmo no inicio(0) e fim(length + 2) da String)
+        \B (not word boundary)
+        . Qualquer charactere
+        [abc] - a ou b ou c
+        [a-c] - a ou b ou c
+        [a-cA-C] - aA ou bB ou cC
+        + - Um ou mais
+        * - Zero ou mais
+        ? - Zero ou um
+        (regex)quantifier - aplica o quantifier no regex dentro dos parenteses.
+
+        Pattern pattern = Pattern.compile("\\b");
+        Matcher matcher = pattern.matcher("value");
+        boolean anyMatches = matcher.matches();
+        while(matcher.find()) {
+            System.out.print(matcher.start()+","+matcher.group());
+        }
+
+        Tokenizing
+
+        String[] tokens = "someString".split("tokenRegex");
+
+        Scanner scanner = new Scanner(someInputStream);
+        scanner.useDelimiter("regex");//Default delimiter espaço em branco
+        scanner.useDelimiter(pattern);
+        boolean hasNextToken = scanner.hasNext();
+        String token = scanner.next();
+        boolean hasNextToken = scanner.hasNextInt();
+        Integer token = scanner.nextInt();
+        ... todos os tipos wrapper menos Character(char)
+
+         StringTokenizer stringTokenizer = new StringTokenizer("someString", "delimiter");
+        if(stringTokenizer.hasMoreTokens()){
+            String token = stringTokenizer.nextToken();
+        }
+
+        Formatting
+
+        %[index][flags][width][.precision]conversionChar
+
+        Conversion Chars
+
+        b - boolean
+        c - char
+        d - integer
+        f - float/double
+        s - String
+
+        Flags
+
+        - - Left justify
+        + - Sinal do número + ou -
+        0 - 0 padding
+        , - Formatar número com pontos e virgulas de acordo com o locale
+        ( - Coloca numero negativo entre parenteses
+
+         System.out.format("%+0,(40.3f", -123456.78910d);
+
+        Resource bundles
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("bundleName", locale);
+        resourceBundle.getString("key");
+
+        Prioridade de busca(sendo que se encontrou java passa a procurar somente java e o mesmo properties)
+
+        bundleName_pt_BR.java
+        bundleName_pt.java
+        bundleName.java
+
+        bundleName_pt_BR.properties
+        bundleName_pt.properties
+        bundleName.properties
+
+
+
+
+
+        */
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("bundleName", locale);
+        resourceBundle.getString("key");
+
+
+
+
+
     }
 
     public static boolean method() throws IOException, SQLException{throw new IOException();}
